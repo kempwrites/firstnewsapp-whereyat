690af4410516dabd14dd952318dbb87fc1f46d01
kempwrites <kempwrites@comcast.net>
2015-03-07 06:19:24 -0800

    leftovers day one

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 
 def get_csv():
 	csv_path = "./static/la-riots-deaths.csv"
-	csv_file = open(csv_path, 'rb')
+	csv_file = open(csv_path, 'r')
 	csv_obj = csv.DictReader(csv_file)
 	csv_list = list(csv_obj)
 	return csv_list
 
 
-@app.route('/')
-def index():
-	template = 'index.html'
+@app.route('/<row_id>')
+def detail(row_id):
+	template = 'detail.html'
 	object_list = get_csv()
 	return render_template(template, object_list=object_list)
 
